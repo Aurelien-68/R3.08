00acc9d80c091982272723f1702a5c137179fdb0
e2400575 <aurelienhenry9@gmail.com>
2025-09-04 09:42:27 +0200

    b

--- TD1/EX1.2.py
+++ TD1/EX1.2.py
@@ -2,24 +2,28 @@ def plus_grand(a: float, b: float) -> float:
     """
     Retourne le plus grand des deux nombres réels a et b.
     """
+    print(max(a, b))
     return max(a, b)
 
 def superieur_a_seuil(valeur: float, seuil: float = 10) -> bool:
     """
     Vérifie si la valeur passée est supérieure au seuil donné (par défaut 10).
     """
+    print(valeur > seuil)
     return valeur > seuil
 
 def plus_grand_liste(valeurs: list[float]) -> float:
     """
     Retourne la plus grande valeur d'une liste de nombres réels.
     """
+    print(max(valeurs))
     return max(valeurs)
 
 def nombre_inférieur_a_seuil(valeurs: list[float], seuil: float = 3) -> int:
     """
     Retourne le nombre de valeurs dans la liste inférieures au seuil donné (par défaut 3).
     """
+    print(sum(1 for v in valeurs if v < seuil))
     return sum(1 for v in valeurs if v < seuil)
 
 def afficher_dictionnaire(dictionnaire: dict) -> None:
@@ -28,3 +32,10 @@ def afficher_dictionnaire(dictionnaire: dict) -> None:
     """
     for cle, valeur in dictionnaire.items():
         print(f"{cle}: {valeur}")
+
+
+plus_grand(15,55)
+superieur_a_seuil(15)
+plus_grand_liste([15,2,44,223,12,56,98])
+nombre_inférieur_a_seuil([1,3,45,78,2,1,2,0.12,2.22])
+afficher_dictionnaire({"nom": "Alice","âge": 30,"ville": "Paris"})
---
+++ TD1/EX2.2.py
@@ -0,0 +1,46 @@
+class Tasse:
+    matière: str = "céramique"
+
+    def __init__(self, couleur: str, contenance: int, marque: str, contenu: str=None):
+        self.couleur = couleur
+        self.contenance = contenance
+        self.marque = marque
+        self.contenu = contenu
+
+    def __str__(self) -> str:
+        if self.contenu:
+            return f"La tasse de matière {self.matière}, de couleur {self.couleur} et de marque {self.marque} a une contenance de {self.contenance} ml et contient {self.contenu}."
+        else:
+            return f"La tasse de matière {self.matière}, de couleur {self.couleur} et de marque {self.marque} a une contenance de {self.contenance} ml et est vide."
+
+    def definir_contenu(self, contenu: str):
+        self.contenu = contenu
+
+    def boire(self):
+        if self.contenu:
+            self.contenu=None
+        else:
+            print("La tasse est déjà vide.")
+
+# Création de plusieurs objets de la classe Tasse
+tasse1 = Tasse("bleue", 250, "Duralex", "coca")
+tasse2 = Tasse("rouge", 350, "IKEA", "eau")
+
+# Affichage des objets avant d'ajouter du contenu
+print(tasse1)
+print(tasse2)
+
+# Définir le contenu des tasses
+tasse1.definir_contenu("café")
+tasse2.definir_contenu("thé")
+
+# Affichage après avoir ajouté du contenu
+print(tasse1)
+print(tasse2)
+
+# Boire le contenu de la tasse
+tasse1.boire()
+print(tasse1)
+
+# Essayer de boire à nouveau alors que la tasse est déjà vide
+tasse1.boire()
