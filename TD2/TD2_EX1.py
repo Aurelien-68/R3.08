f21e86ea28f9d53f9c704df729a27b46e39d9eb2
e2400575 <aurelienhenry9@gmail.com>
2025-10-10 14:21:27 +0200

    dd

--- TD2/TD2_EX1.py
+++ TD2/TD2_EX1.py
@@ -1,6 +1,12 @@
-def plus_grand(a: float, b: float) -> float:
+def division_par_2(a: float, b: float) -> float:
     try:
-        resultat=
+        return a/b
+    except TypeError:
+        print("a ou b ne sont pas des nombre réel")
+    except ZeroDivisionError:
+        print("ATTENTION b est egal a zero, la division est imposilble")
 
 
-    except:
+print(division_par_2(10, 2))   # 5.0
+print(division_par_2(10, 0))   # message d'erreur + None
+print(division_par_2(10, "a")) # message d'erreur + None
